0483acbc469b4be096569b58f3d03d28410fd68f
Brandon Martin <martinb@psd401.net>
2015-03-19 09:02:44 -0700

    Added more events to client, changed from PNG to JPEG for performance

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var rfb = require('rfb2'),
   port = 8090,
   socketIoPort = 8091,
   socketio = require('socket.io').listen(socketIoPort, { log: false }),
-  Png = require('./node_modules/node-png/build/Release/png').Png,
+ // Png = require('./node_modules/node-png/build/Release/png').Png,
+  Png = require('./node_modules/node-jpeg/build/Release/jpeg').Jpeg,
   connect = require('connect'),
   clients = [];
 
@@ -18,6 +19,7 @@ function createRfbConnection(config, socket) {
 
 function addEventHandlers(r, socket) {
   r.on('connect', function () {
+    console.log("Connected to RFB");
     socket.emit('init', {
       width: r.width,
       height: r.height
@@ -27,13 +29,22 @@ function addEventHandlers(r, socket) {
       rfb: r
     });
   });
+
   r.on('rect', function (rect) {
     handleFrame(socket, rect, r);
     r.requestUpdate(false, 0, 0, r.width, r.height);
   });
+
+  r.on('data', function(data){
+//	console.log("Got some Data!");
+  });
+
 }
 
 function handleFrame(socket, rect, r) {
+ // socket.emit('rect', rect);
+
+ 
   var rgb = new Buffer(rect.width * rect.height * 3, 'binary'),
     offset = 0;
 
@@ -42,14 +53,18 @@ function handleFrame(socket, rect, r) {
     rgb[offset++] = rect.data[i + 1];
     rgb[offset++] = rect.data[i];
   }
+
   var image = new Png(rgb, r.width, r.height, 'rgb');
-  image = image.encodeSync();
-  socket.emit('frame', {
-    x: rect.x,
-    y: rect.y,
-    width: rect.width,
-    height: rect.height,
-    image: image.toString('base64')
+  image.encode(function(png, dms){
+  //console.log("Emmitting frame");
+  	socket.emit('frame', {
+  	  x: rect.x,
+    	  y: rect.y,
+   	  width: rect.width,
+   	  height: rect.height,
+          image: png.toString('base64')
+    //      rgb: rgb
+  	});
   });
 }
 
@@ -70,7 +85,9 @@ socketio.sockets.on('connection', function (socket) {
   socket.on('init', function (config) {
     var r = createRfbConnection(config, socket);
     socket.on('mouse', function (evnt) {
+ //     console.log(evnt.x);
       r.pointerEvent(evnt.x, evnt.y, evnt.button);
+	
     });
     socket.on('keyboard', function (evnt) {
       r.keyEvent(evnt.keyCode, evnt.isDown);
